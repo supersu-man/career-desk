95c2a7124ee08ac3d6f1bf302885acadacb773f2
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2025-10-18 15:12:02 +0530

    feat: scrape hyland

--- src/electron/scrapers.ts
+++ src/electron/scrapers.ts
@@ -7,7 +7,8 @@ export const scrapers: Record<string, ScraperFn> = {
     verizon: scrapeVerizon,
     ea: scrapeEA,
     ibm: scrapeIBM,
-    amazon: scrapeAmazon
+    amazon: scrapeAmazon,
+    hyland: scrapeHyland
 };
 
 async function scrapeWorkday(company: Company, options: ScraperOptions): Promise<JobPosting[]> {
@@ -160,4 +161,33 @@ async function scrapeAmazon(company: Company, options: ScraperOptions): Promise<
         location: job.normalized_location,
         postedOn: job.posted_date
     }))
+}
+
+
+async function scrapeHyland(company: Company, options: ScraperOptions): Promise<JobPosting[]> {
+    console.log(`Scraping ${company.companyName}`)
+    const headers = {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36"
+    }
+    const params: any = { in_iframe: 1 }
+    if(options.country) params.searchLocation = options.country
+    if(options.query) params.searchKeyword = options.query
+    const response = await axios.get( company.endpoint, { params, headers } )
+    const $ = cheerio.load(response.data)
+    const jobs: JobPosting[] = []
+    $('.iCIMS_JobsTable .row').each((_, el) => {
+        const href = $(el).find('a').attr('href') as string;
+        const title = $(el).find('h3').text()
+        const location = $(el).find('.iCIMS_JobHeaderTag .iCIMS_JobHeaderData').eq(2).text().trim()
+        jobs.push({
+            title: title,
+            url: href,
+            location: location,
+            saved: false,
+            applied: false,
+            company: company.companyName
+        })
+    })
+    return jobs
 }
